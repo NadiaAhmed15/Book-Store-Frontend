818ef08b9842a7bda7f1a0317e724427df212dc1
NadiaAhmed15 <nadiaahmedoo15@gmail.com>
2025-01-16 23:34:59 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
 // export const SERVER_URL = "http://localhost:5555";
-export const SERVER_URL = "https://book-store-backend-gamma-two.vercel.app";
+export const SERVER_URL = "https://book-store-backend-one-gamma.vercel.app";
